5b47623db9097c247315d9aded58b8b9efe6f3ec
Devendra116 <chauhandevendra116@gmail.com>
2023-06-13 19:49:40 +0530

    fix: add db connection

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,7 @@
 import { User } from "@/lib/model";
+import { signJwtAccessToken } from "@/lib/jwt";
 import * as bcrypt from "bcrypt";
+import { connectToDatabase } from "@/lib/database/connection";
 
 interface RequestBody {
   email: string;
@@ -7,14 +9,18 @@ interface RequestBody {
 }
 export async function POST(request: Request) {
   const body: RequestBody = await request.json();
-
+  await connectToDatabase()
   const user = await User.findOne({
       email: body.email,
   });
 
   if (user && (await bcrypt.compare(body.password, user.password))) {
     const { password,_id, ...userWithoutPass } = user.toObject();
-    
-    return new Response(JSON.stringify(userWithoutPass));
+    const accessToken = signJwtAccessToken(userWithoutPass);
+    const result = {
+      ...userWithoutPass,
+      accessToken,
+    };
+    return new Response(JSON.stringify(result));
   } else return new Response(JSON.stringify(null));
 }
