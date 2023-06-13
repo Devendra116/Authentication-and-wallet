f2d99cd45efb5f1d3a485102f359c716d774f324
Devendra116 <chauhandevendra116@gmail.com>
2023-06-14 00:10:57 +0530

    add navbar

---
+++ src/component/AppBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import SigninButton from "./SignInButton";
+
+const AppBar = () => {
+    return (
+        <header className="flex  items-center justify-between p-4 bg-gradient-to-b from-white to-gray-200 shadow">
+            <div className="flex  gap-4 justify-end w-full">
+                <div>
+                    <Link className="transition-colors hover:text-blue-500" href={"/"}>
+                        Home
+                    </Link>
+                </div>
+            </div>
+
+            <div>
+                <SigninButton />
+            </div>
+        </header>
+    );
+};
+
+export default AppBar;
